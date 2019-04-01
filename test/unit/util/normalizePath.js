9505979403a154a5925d7a6a132296739a71f8cf
Nick K <nick.kruchinin@gmail.com>
2019-04-01 22:09:13 +0300

    Add tests for normalizePath util.

---
+++ test/unit/util/normalizePath.js
@@ -0,0 +1,40 @@
+const {join} = require("path")
+
+const test = require("ava")
+
+const {normalizePath} = require("../../../lib/util/normalizePath")
+
+const fixtures = join(__dirname, "..", "..", "fixture")
+
+test("Returns absolute path as-is", async t => {
+  const expected = join(fixtures, "read/file.yml")
+  const actual = await normalizePath("/", expected)
+
+  t.is(actual, expected)
+})
+
+test("Use given base to resolve relative path", async t => {
+  const expected = join(fixtures, "read/file.yml")
+  const actual = await normalizePath(fixtures, "read/file.yml")
+
+  t.is(actual, expected)
+})
+
+test("Correctly resolves file path without file extension", async t => {
+  const expected = join(fixtures, "read/file.yml")
+  const actual = await normalizePath(fixtures, "read/file")
+
+  t.is(actual, expected)
+})
+
+test("Returns given path when can't match any file", async t => {
+  const actual = await normalizePath(fixtures, "foo")
+
+  t.is(actual, join(fixtures, "foo"))
+})
+
+test("Throws an error when unsupported file extension given", async t => {
+  const path = join("read", "file.unknown")
+
+  await t.throwsAsync(normalizePath(fixtures, path))
+})
---
+++ test/unit/util/normalizePathSync.js
@@ -0,0 +1,42 @@
+const {join} = require("path")
+
+const test = require("ava")
+
+const {normalizePathSync} = require("../../../lib/util/normalizePath")
+
+const fixtures = join(__dirname, "..", "..", "fixture")
+
+test("Returns absolute path as-is", t => {
+  const expected = join(fixtures, "read/file.yml")
+  const actual = normalizePathSync("/", expected)
+
+  t.is(actual, expected)
+})
+
+test("Use given base to resolve relative path", t => {
+  const expected = join(fixtures, "read/file.yml")
+  const actual = normalizePathSync(fixtures, "read/file.yml")
+
+  t.is(actual, expected)
+})
+
+test("Correctly resolves file path without file extension", t => {
+  const expected = join(fixtures, "read/file.yml")
+  const actual = normalizePathSync(fixtures, "read/file")
+
+  t.is(actual, expected)
+})
+
+test("Returns given path when can't match any file", t => {
+  const actual = normalizePathSync(fixtures, "foo")
+
+  t.is(actual, join(fixtures, "foo"))
+})
+
+test("Throws an error when unsupported file extension given", t => {
+  const path = join("read", "file.unknown")
+
+  const trap = () => normalizePathSync(fixtures, path)
+
+  t.throws(trap)
+})
